8d47f3ff57f3aeed8fd75ed9dbee8082e072aca4
monkey999por <34706709+monkey999por@users.noreply.github.com>
2022-10-26 13:37:06 +0900

    ok

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -9,9 +9,15 @@ export default function FirstPost() {
     return (
         <>
             <Layout>
-                <Head>
+                <Head >
                     <title>First Post</title>
                 </Head>
+                <Script 
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad={() => {
+                    console.log('onload hogehogehge')
+                }}></Script>
                 <h1>First post</h1>
                 <h2>
                     <Link href="/">Back to home</Link>
